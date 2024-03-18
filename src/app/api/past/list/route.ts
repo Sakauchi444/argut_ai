62c0e248f25cb7117f1aa3e17f2a323f6266b3f0
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2024-03-18 16:49:20 +0900

    Rename: past/list/route.ts

---
+++ src/app/api/past/list/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const response = await fetch(
+      "https://d2ycluine3obbg.cloudfront.net/past/list"
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    const data = await response.json();
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return NextResponse.error();
+  }
+}
