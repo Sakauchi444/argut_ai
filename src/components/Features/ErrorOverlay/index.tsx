6cb441922dd3d328b9ab0fea15cdd5005bb3726a
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2024-03-19 05:15:41 +0900

    add: reload button

--- src/components/Features/ErrorOverlay/index.tsx
+++ src/components/Features/ErrorOverlay/index.tsx
@@ -1,9 +1,11 @@
-import { Center, Overlay } from "@mantine/core";
+import { Button, Center, Overlay, Title } from "@mantine/core";
 import Image from "next/image";
 import React from "react";
 import classes from "./errorOverlay.module.css";
+import { useRouter } from "next/navigation";
 
 const ErrorOverlay = () => {
+	const router = useRouter();
 	return (
 		<Overlay zIndex={3000}>
 			<Center h={"100%"} pos={"relative"} />
@@ -12,8 +14,13 @@ const ErrorOverlay = () => {
 				alt="panic"
 				fill
 				sizes="50vw"
+				style={{ objectFit: "contain" }}
 				className={classes.spin_and_shrink}
 			/>
+			<Title>
+				ずんだもんの頭がパンクしてしまいました。もう一度お試しください。
+				<Button onClick={() => router.refresh()}>Reload</Button>
+			</Title>
 		</Overlay>
 	);
 };
